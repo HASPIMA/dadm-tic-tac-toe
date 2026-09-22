803e8ada71a085a58098b78652781f25463840db
Harrison Pinto <hpintom@unal.edu.co>
2026-09-22 18:53:59 -0500

    Create `Paint` variable in `BoardView`
    This will help us control the color and thickness of the lines drawn on
    the board

--- app/src/main/java/com/edu/unal/tictactoe/BoardView.java
+++ app/src/main/java/com/edu/unal/tictactoe/BoardView.java
@@ -3,19 +3,24 @@ package com.edu.unal.tictactoe;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class BoardView extends View {
     // Width of the board grid lines
-    public static final int  GRID_WIDTH = 6;
+    public static final int GRID_WIDTH = 6;
 
     private Bitmap humanBitmap;
     private Bitmap computerBitmap;
 
+    // Controls the color and thickness of the lines drawn on the board
+    private Paint paint;
+
     public void initialize() {
         humanBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.x_img);
         computerBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.o_img);
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
     public BoardView(Context context) {
