10ddb671bb9d527d3109b8f33b2b11970cd6d385
Harrison Pinto <hpintom@unal.edu.co>
2026-09-22 19:53:00 -0500

    Migrate `BoardView` to Kotlin

---
+++ app/src/main/java/com/edu/unal/tictactoe/BoardView.kt
@@ -0,0 +1,123 @@
+package com.edu.unal.tictactoe
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.View
+
+class BoardView : View {
+    private var humanBitmap: Bitmap? = null
+    private var computerBitmap: Bitmap? = null
+
+    // Controls the color and thickness of the lines drawn on the board
+    private var paint: Paint? = null
+
+    private var game: TicTacToeGame? = null
+    private val drawingRect = Rect()
+
+    fun initialize() {
+        humanBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.x_img)
+        computerBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.o_img)
+        paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    }
+
+    fun setGame(game: TicTacToeGame?) {
+        this.game = game
+    }
+
+    val boardCellWidth: Int
+        get() = getWidth() / 3
+
+    val boardCellHeight: Int
+        get() = getHeight() / 3
+
+    override fun performClick(): Boolean {
+        return super.performClick()
+    }
+
+    constructor(context: Context?) : super(context) {
+        initialize()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        initialize()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        initialize()
+    }
+
+    public override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        // Determine the width and height of the View
+        val boardWidth = getWidth()
+        val boardHeight = getHeight()
+
+        // Define lines' color and thickness
+        paint!!.setColor(Color.LTGRAY)
+        paint!!.setStrokeWidth(GRID_WIDTH.toFloat())
+
+        // Draw the two vertical board lines
+        val cellWidth = boardWidth / 3
+        canvas.drawLine(
+            cellWidth.toFloat(), 0f,
+            cellWidth.toFloat(), boardHeight.toFloat(),
+            paint!!
+        )
+        canvas.drawLine(
+            (cellWidth * 2).toFloat(), 0f,
+            (cellWidth * 2).toFloat(), boardHeight.toFloat(),
+            paint!!
+        )
+
+        // Draw the two horizontal board lines
+        val cellHeight = boardHeight / 3
+        canvas.drawLine(
+            0f, cellHeight.toFloat(),
+            boardWidth.toFloat(), cellHeight.toFloat(),
+            paint!!
+        )
+        canvas.drawLine(
+            0f, (cellHeight * 2).toFloat(),
+            boardWidth.toFloat(), (cellHeight * 2).toFloat(),
+            paint!!
+        )
+
+        // Draw all the X and O images
+        if (game != null) {
+            for (i in 0..<TicTacToeGame.BOARD_SIZE) {
+                val col = i % 3
+                val row = i / 3
+
+                val left: Int = col * cellWidth + GRID_WIDTH
+                val top: Int = row * cellHeight + GRID_WIDTH
+                val right: Int = (col + 1) * cellWidth - GRID_WIDTH
+                val bottom: Int = (row + 1) * cellHeight - GRID_WIDTH
+
+                val occupant = game!!.getBoardOccupant(i)
+                if (occupant == TicTacToeGame.HUMAN_PLAYER) {
+                    drawingRect.set(left, top, right, bottom)
+                    canvas.drawBitmap(humanBitmap!!, null, drawingRect, null)
+                } else if (occupant == TicTacToeGame.COMPUTER_PLAYER) {
+                    drawingRect.set(left, top, right, bottom)
+                    canvas.drawBitmap(computerBitmap!!, null, drawingRect, null)
+                }
+            }
+        }
+    }
+
+    companion object {
+        // Width of the board grid lines
+        const val GRID_WIDTH: Int = 6
+    }
+}
