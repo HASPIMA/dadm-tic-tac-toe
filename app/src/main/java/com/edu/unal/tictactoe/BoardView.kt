280d69ae60d8b74b94215652ee165574b5615a54
Harrison Pinto <hpintom@unal.edu.co>
2026-09-22 20:01:51 -0500

    Remove nullability for some fields in `BoardView`
    This guarantees that `paint`, `computerBitmap` and `humanBitmap` are
    non-null.

--- app/src/main/java/com/edu/unal/tictactoe/BoardView.kt
+++ app/src/main/java/com/edu/unal/tictactoe/BoardView.kt
@@ -13,11 +13,11 @@ import android.util.Log
 import android.view.View
 
 class BoardView : View {
-    private var humanBitmap: Bitmap? = null
-    private var computerBitmap: Bitmap? = null
+    private var humanBitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.x_img)
+    private var computerBitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.o_img)
 
     // Controls the color and thickness of the lines drawn on the board
-    private var paint: Paint? = null
+    private var paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     private var game: TicTacToeGame? = null
     private val drawingRect = Rect()
